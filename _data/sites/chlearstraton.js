2a8923be58ff9fcd87ee665d0baaddedbf0414cc
Sachin Sancheti <sachin.sancheti@gmail.com>
2020-11-22 15:01:42 +0530

    added site pages

---
+++ _data/sites/chlearstraton.js
@@ -0,0 +1,19 @@
+module.exports = {
+	name: "Chlear Straton Group", // optional, falls back to object key
+	description: "The default sites that get tested",
+	options: {
+		frequency: 60 * 24, // (in minutes), 24 hours
+	},
+	urls: [
+		"https://www.chlear.in/test/straton/beta/",
+		"https://www.chlear.in/test/straton/beta/aboutus.html",
+		"https://www.chlear.in/test/straton/beta/wpc-decking-cladding-louvers-pergola.html",
+		"https://www.chlear.in/test/straton/beta/ventilated-facade-cladding-hpl.html",
+		"https://www.chlear.in/test/straton/beta/pvc-boards-plywood-replacement.html",
+		"https://www.chlear.in/test/straton/beta/toilet-cubicle-solution.html",
+		"https://www.chlear.in/test/straton/beta/clientele.html",
+		"https://www.chlear.in/test/straton/beta/blog.html",
+		"https://www.chlear.in/test/straton/beta/careers.html",
+		"https://www.chlear.in/test/straton/beta/contact-us.html"
+	]
+};
--- _data/sites/ssg.js
+++ _data/sites/ssg.js
@@ -4,7 +4,7 @@ module.exports = {
 	// skip if localhost
 	// skip if this is a new fork of the speedlify (not Zach’s)
 	options: {
-		frequency: 60 * 23, // 24 hours
+		frequency: 60 * 24 * 7, // 24 hours
 		// Use "run" if the sites don’t share assets on the same origin
 		//           and we can reset chrome with each run instead of
 		//           each site in every run (it’s faster)
@@ -17,6 +17,17 @@ module.exports = {
 		"https://vitraggroup.com/gated-community/",
 		"https://vitraggroup.com/plots-for-sale/",
 		"https://vitraggroup.com/about/",
-		"https://vitraggroup.com/tags/"
+		"https://vitraggroup.com/tags/",
+		"https://vitraggroup.com/houses-for-sale/balaclava-large-house-coonoor/",
+		"https://vitraggroup.com/houses-for-sale/malhar-cottage-tea-garden-coonoor-kotagiri/",
+		"https://vitraggroup.com/gated-community/hill-valley-enclave-wellington-municipal-property/",
+		"https://vitraggroup.com/gated-community/serenitea-gated-community-ooty-coonoor/",
+		"https://vitraggroup.com/plots-for-sale/plot-12-at-creekside/",
+		"https://vitraggroup.com/plots-for-sale/plot-52-at-serenitea/",
+		"https://vitraggroup.com/plots-for-sale/plot-bettati-g-at-independent-plots/",
+		"https://vitraggroup.com/tags/communities-new/",
+		"https://vitraggroup.com/archive-of-properties/",
+		"https://vitraggroup.com/houses-for-sale/jannat-villa-sua-serenitea-coonoor/",
+		"https://vitraggroup.com/our-testimonials/"
 	]
 };
