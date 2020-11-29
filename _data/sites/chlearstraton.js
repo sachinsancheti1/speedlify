06b9360e14f1a7b6ac9213f88c1f53a07b43933a
Sachin Sancheti <sachin.sancheti@gmail.com>
2020-11-29 23:57:34 +0530

    Update chlearstraton.js

--- _data/sites/chlearstraton.js
+++ _data/sites/chlearstraton.js
@@ -8,9 +8,9 @@ module.exports = {
 		"https://www.chlear.in/test/straton/beta/",
 		"https://www.chlear.in/test/straton/beta/aboutus.html",
 		"https://www.chlear.in/test/straton/beta/wpc-decking-cladding-louvers-pergola.html",
-		"https://www.chlear.in/test/straton/beta/ventilated-facade-cladding-hpl.html",
+
 		"https://www.chlear.in/test/straton/beta/pvc-boards-plywood-replacement.html",
-		"https://www.chlear.in/test/straton/beta/toilet-cubicle-solution.html",
+	
 		"https://www.chlear.in/test/straton/beta/clientele.html",
 		"https://www.chlear.in/test/straton/beta/blog.html",
 		"https://www.chlear.in/test/straton/beta/careers.html",
