b0c6d913500ba9e264f63a1104c9369df806a219
Sachin Sancheti <sachin.sancheti@gmail.com>
2020-12-15 19:38:08 +0530

    Update chlearstraton.js

--- _data/sites/chlearstraton.js
+++ _data/sites/chlearstraton.js
@@ -2,18 +2,18 @@ module.exports = {
 	name: "Chlear Straton Group", // optional, falls back to object key
 	description: "Chlear Straton Group",
 	options: {
-		frequency: 60 * 24, // (in minutes), 24 hours
+		frequency: 60 * 12, // (in minutes), 24 hours
 	},
 	urls: [
-		"https://www.chlear.in/test/straton/beta/",
-		"https://www.chlear.in/test/straton/beta/aboutus.html",
-		"https://www.chlear.in/test/straton/beta/wpc-decking-cladding-louvers-pergola.html",
+		"https://www.stratongroup.com/",
+		"https://www.stratongroup.com/aboutus.html",
+		"https://www.stratongroup.com/wpc-decking-cladding-louvers-pergola.html",
 
-		"https://www.chlear.in/test/straton/beta/pvc-boards-plywood-replacement.html",
+		"https://www.stratongroup.com/pvc-boards-plywood-replacement.html",
 	
-		"https://www.chlear.in/test/straton/beta/clientele.html",
-		"https://www.chlear.in/test/straton/beta/blog.html",
-		"https://www.chlear.in/test/straton/beta/careers.html",
-		"https://www.chlear.in/test/straton/beta/contact-us.html"
+		"https://www.stratongroup.com/clientele.html",
+		"https://www.stratongroup.com/blog.html",
+		"https://www.stratongroup.com/careers.html",
+		"https://www.stratongroup.com/contact-us.html"
 	]
 };
